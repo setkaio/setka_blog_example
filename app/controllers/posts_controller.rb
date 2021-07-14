9ace95d4eb0af9a0a10bc9672a8142c985608ee8
Mikhail Zadera <m_zadera@setka.io>
2021-07-14 19:18:24 +0300

    CRUD posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    Post.find(params[:id]).update(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:id, :title, :body)
+  end
+end
