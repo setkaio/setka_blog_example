a2537931659f6c5bad0e724045c73a267fcd3aea
Mikhail Zadera <m_zadera@setka.io>
2021-08-03 15:32:43 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Post.create([
+    {
+      title: Faker::Lorem.sentence,
+      body: <<-EOS
+        <div class=\"stk-post stk-layout_4col_12670 stk-theme_44132\" data-ui-id=\"post\" data-reset-type=\"class\" data-editor-version=\"3.0.14\" data-stk=\"{&quot;images&quot;:[]}\" data-stk-css=\"stkgTT2P\" data-layout-type=\"limited\">\
+        <p class=\"stk-reset\" data-ce-tag=\"paragraph\">#{Faker::Lorem.paragraphs.join(' ')}</p>\
+        <p class=\"stk-theme_44132__color_44132_custom_color_1 stk-reset\" data-ce-tag=\"paragraph\">#{Faker::Lorem.paragraphs.join(' ')}</p>\
+        <style data-stk-css=\"stkgTT2P\" class=\"\" media=\"all\">[data-stk-css=\"stkgTT2P\"]:not(#stk):not(#stk):not(style){background-color: rgba(255, 229, 49, 1.0)}</style>\
+        </div>
+      EOS
+    }
+  ])
+end
